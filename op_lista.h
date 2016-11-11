793633f529cfa2bab760d02108b49b312114c52e
AxeOfAssis <nyxforlol3@gmail.com>
2016-11-11 16:39:07 -0200

    Update op_lista.h

--- op_lista.h
+++ op_lista.h
@@ -1,5 +1,11 @@
+#pragma once
 #include "lista.h"
+#define TRUE 1
+#define FALSE 0
 #define ERRO_MEMORIA -1
+#define ERR0_NUMERO_INVALIDO -2
 
 void adiciona(LDE *l1, LDE *l2, LDE *l3);
 void multiplica(LDE *l1, LDE *l2, LDE *l3);
+int inicializa_numero(LDE *l1, char *num);
+int verifica_caracteres(char *num);
