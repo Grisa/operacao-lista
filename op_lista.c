8eccc0c6b88b9ee338da3aebcf9f56085654058d
AxeOfAssis <nyxforlol3@gmail.com>
2016-11-16 17:01:43 -0200

    Update op_lista.c

--- op_lista.c
+++ op_lista.c
@@ -24,18 +24,33 @@ int verifica_caracteres(char *num)
 	return TRUE; // OK;
 }
 
-void adiciona(LDE * l1, LDE * l2, LDE * l3){
+LDE *adiciona(LDE *l1,int pos1,int pos2){
 	
 	/*Chegar até o final da L1 e L2 recursivamente para fazer uma pilha implicita
+
+	igualar numero de digitos inserindo 0 no começo
 
 	adicionar info das listas junto com sobra
 
 	criar um Elemento para cada soma de l1 e l2 (junto com a sobra) e inserir no inicio da lista;
 	*/
 
+	ElementoDuplo *temp = l1->cabeca;
+
+	for (int i = 0; i < pos1; i++)
+	{
+		if (temp->proximo != NULL) temp = temp->proximo;
+	}
+	for (int i = pos1; i <= pos2; i++){
+		LDE *sub = malloc(sizeof(LDE));
+		leNaPosicao(l1, sub, i);
+		printf("quantidade de elementos na lista %d === %d\n",i+1,sub->quantidade_elementos);
+		
+	}
+
 }
 
-void multiplica(LDE * l1, LDE * l2, LDE * l3){
+LDE *multiplica(LDE * l1, LDE * l2){
 
 }
 
